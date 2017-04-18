3d23cb4143d074df0d71ddee6f6d5b948023581e
masayuki yamaguchi <ymgc.msyk31@gmail.com>
2017-04-18 04:57:24 +0000

    modified webpage form and layout

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 class TasksController < ApplicationController
-#  before_action :set_task, only: [:show, :edit, :update, :destroy]
+ before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = Task.all.page(params[:page]).per(10)
   end
   
   def show
-    set_task
   end
   
   def new
@@ -26,12 +25,10 @@ class TasksController < ApplicationController
   end
   
   def edit
-    set_task
   end
   
   def update
-    set_task
-    if @task.update(task_params)
+      if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
     else
@@ -41,7 +38,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    set_task
     @task.destroy
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
